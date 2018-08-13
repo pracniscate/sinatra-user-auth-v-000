4b293829a87f4f2689ce934322b6088e075e0abd
Lorena <lorena.giddens@outlook.com>
2018-08-13 22:09:36 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,30 +11,35 @@ class ApplicationController < Sinatra::Base
     erb :home
   end
 
-  get '/registrations/signup' do
+  get '/registrations/signup' do #render the sign-up form view
     erb :'/registrations/signup'
   end
 
-  post '/registrations' do
-
+  post '/registrations' do #handles the post request when user submits the sign-up form
+    #get the new user's info from the params hash
+    #create a new user
+    #sign in the user
     redirect '/users/home'
   end
 
-  get '/sessions/login' do
+  get '/sessions/login' do #renders the login form
     erb :'sessions/login'
   end
 
-  post '/sessions' do
+  post '/sessions' do #receives the post request when user submitted the login form
+    #grab the user's info from the params hash 
+    #look to match the info against the existing entries in the user database
+    #if a match is found, the user is signed in
 
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
+  get '/sessions/logout' do #logs the user out by clearing the session hash
 
     redirect '/'
   end
 
-  get '/users/home' do
+  get '/users/home' do #renders the user's homepage view
 
     erb :'/users/home'
   end
